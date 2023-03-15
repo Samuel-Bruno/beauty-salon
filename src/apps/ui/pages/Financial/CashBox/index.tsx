5c00b4f92e6c2005d7827c0560b1197b2bf9d635
Samuel Bruno <samdg919@gmail.com>
2023-03-15 15:08:30 -0300

    Fixed Financial CashBox

--- src/apps/ui/pages/Financial/CashBox/index.tsx
+++ src/apps/ui/pages/Financial/CashBox/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import * as S from './styles'
 
-import { FilterArea } from '../styles'
+import Button from '../../../components/Buttons'
 
 import { ReactComponent as ArrowIcon } from "../../../assets/icons/arrow.svg"
 
@@ -18,9 +18,17 @@ const CashBox = () => {
           <span>Caixa</span>
         </S.PageTitle>
       </S.Header>
-      <FilterArea>
-        Caixa
-      </FilterArea>
+      <S.Principal>
+        <h2>O caixa está fechado. O que você deseja fazer?</h2>
+        <S.Options>
+          <Button type='model2' title='Abrir novo Caixa' />
+          <Button type='model1' title='Reabrir último caixa' />
+          <Button type='model1' title='Reabrir caixa antigo' />
+          <Button type='model1' title='Cadastrar caixa antigo' />
+          <Button type='model1' title='Ver caixas fechados' />
+          <Button type='model1' title='Fluxo de caixa' />
+        </S.Options>
+      </S.Principal>
     </>
   )
 
